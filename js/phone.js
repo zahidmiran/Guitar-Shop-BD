334a320a47314286be16f990aa7b7fda025e1eab
zahidmiran <zahidmiron599@gmail.com>
2022-03-01 21:47:30 +0600

    spiner added

--- js/phone.js
+++ js/phone.js
@@ -1,44 +1,30 @@
-// console.log('js added');
-//search box//
-const searchPhone = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    // console.log(searchText);
-    searchField.value = '';
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+const searchBtn = document.getElementById('searchBtn');
+const containerData = document.getElementById('phone');
+const warning = document.getElementById('warning');
+warning.style.display = 'none';
 
-    // console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.data));
+// spinner
+const toggleSpinner = displaySpinner => {
+    document.getElementById('spinner').style.display = displaySpinner;
+
 }
 
-// search result //
-const displaySearchResult = data => {
-    const searchResult = document.getElementById('search-result');
-    data.forEach(data => {
-        // console.log(data);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card">
-            <img src="${data.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${data.phone_name}</h5>
-                <h4 class="card-text">${data.brand}</h4>
-                <button id="details-btn" onclick="displayDetail(${data.slug}}" type="button" class="btn btn-primary">Primary</button>
+searchBtn.addEventListener('click',
+    function () {
+        const searchArea = document.getElementById('searchArea').value;
 
-            </div>
-        </div>
-        `;
-        searchResult.appendChild(div);
-    })
-}
-
-const displayDetail = phoneId => {
-    console.log(phoneId);
-    const url = `https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-${data.slug}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data));
-}
+        const searchField = document.getElementById('searchArea');
+        // clear data
+        searchField.value = '';
+        toggleSpinner('block');
+        // document.getElementById('searchArea') = '';
+        containerData.innerHTML = '';
+        if (searchArea === '' && searchArea != getPhones) {
+            warning.style.display = 'block';
+        }
+        else {
+            getPhones(searchArea);
+            warning.style.display = 'none';
+        }
+    }
+);
