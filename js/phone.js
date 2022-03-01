981254379bdd76686a1651fef9cc076e9171153a
zahidmiran <zahidmiron599@gmail.com>
2022-03-01 12:00:01 +0600

    search result added

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
 // console.log('js added');
-
+//search box//
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -10,5 +10,25 @@ const searchPhone = () => {
     // console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+
+// search result //
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${data.phone_name}</h5>
+                <h4 class="card-text">${data.brand}</h4>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
